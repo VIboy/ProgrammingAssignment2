0cf11ba453fd62b39ed9da92447bdc065d1d918d
VIboy <wingchoh@gmail.com>
2016-02-28 16:40:18 +0800

    Latest Update

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -5,19 +5,30 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setmatrix <- function() m <<- solve(x)
+  setmatrix <- function(solve) m <<- solve
   getmatrix <- function() m
   list(set = set, get = get,
        setmatrix = setmatrix,
        getmatrix = getmatrix)
 }
 
-cachesolve <- function(x, ...) {
-  m <- getmatrix()
+# Test
+x <- matrix(1:4,2,2)
+b<-makeCacheMatrix(x)
+cacheSolve(b)
+x * cacheSolve(b)
+cacheSolve(b)
+x
+# End Test     
+
+cacheSolve <- function(x, ...) {
+  m <- x$getmatrix()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
-  m <- solve(x, ...)
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
   m
 }
